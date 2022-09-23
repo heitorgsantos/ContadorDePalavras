f7b9435dfc8960239ae88da9ca383af468c4bb81
heitorgsantos <heitorg33@gmail.com>
2022-09-23 16:26:10 -0300

    Jogo contador de palavras

---
+++ public/js/main.js
@@ -0,0 +1,100 @@
+let tempoInicial = $("#segundos").text();
+let campo = $("#campo-digitacao");
+
+$(function () {
+  tamanhoFrase();
+  contador();
+  iniciaCronometro();
+  colorArea();
+  $("#restart").on("click", restart);
+});
+
+function tamanhoFrase() {
+  let frase = $(".frase").text();
+  let tamanho = frase.split(" ").length;
+  let tamanhoFrase = $("#tamanho-frase");
+  tamanhoFrase.text(tamanho);
+}
+function contador() {
+  campo.on("input", function () {
+    let contar = campo.val();
+    let qltPalavras = contar.split(/\S+/).length - 1;
+    $("#palavras").text(qltPalavras);
+    let qtdCaracteres = contar.length;
+    $("#caracteres").text(qtdCaracteres);
+  });
+}
+
+function colorArea() {
+  let frase = $(".frase").text();
+  campo.on("input", function () {
+    let digitado = campo.val();
+    let comparavel = frase.substr(0, digitado.length);
+    if (digitado == comparavel) {
+      campo.addClass("borda-verde");
+      campo.removeClass("borda-vermelha");
+    } else {
+      campo.removeClass("borda-verde");
+      campo.addClass("borda-vermelha");
+    }
+  });
+}
+
+function iniciaCronometro() {
+  campo.one("focus", function () {
+    let time = $("#segundos").text();
+    const id = setInterval(function () {
+      $("#restart").attr("disabled", true);
+      time--;
+      $("#segundos").text(time);
+      if (time < 1) {
+        placar();
+        campo.attr("disabled", true);
+        campo.addClass("campo-desativado");
+        clearInterval(id);
+        $("#restart").attr("disabled", false);
+      }
+    }, 1000);
+  });
+}
+
+function placar() {
+  let tablePlacar = $("#placar").find("tbody");
+  let numPalavras = $("#palavras").text();
+  let nome = "Heitor";
+  let buttonRemove = "<a href='#'><i class='small material-icons'>delete</i></a>"
+
+  let linha = "<tr>" +
+                "<td>" + nome + "</td>" +
+                "<td>" + numPalavras + "</td>" +
+                "<td>" + buttonRemove + "</td>" +
+              "</tr>";
+  tablePlacar.prepend(linha);
+}
+
+$(".botao-remover").click(function(event) {
+  event.preventDefault();
+  $(this).parent().parent().remove();
+})
+
+
+function novaLinha() {
+  let containerMain = $("<tr>");
+  let nome = $("<td>");
+  let corpo = $("#t-body");
+  $()
+
+
+}
+
+function restart() {
+  campo.attr("disabled", false);
+  campo.val(" ");
+  $("#caracteres").text("0");
+  $("#palavras").text("0");
+  $("#segundos").text(tempoInicial);
+  iniciaCronometro();
+  campo.removeClass("campo-desativado");
+  campo.removeClass("borda-vermelha");
+  campo.removeClass("borda-verde");
+}
